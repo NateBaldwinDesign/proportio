037347392c55fcab2a5a58d317d09c6b84b43276
Nate Baldwin <natebaldwindesign@gmail.com>
2023-04-12 20:28:26 +0000

    Update 3 files

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { useRecoilState } from 'recoil';
 import Logo from "./logo";
+import GitHubLogo from '../icons/github.js'
 
 import "../styles/header.css";
 
 const Header = (props) => {
   return (
     <header>
-      <div className="logoLockup">
-        <Logo color="var(--logoColor)" size="32" strokeWidth="2.5" />
-        <h4 className="logo">Proportio</h4>
+      <div className="header--left">
+        <div className="logoLockup">
+          <Logo color="var(--logoColor)" size="32" strokeWidth="2.5" />
+          <h4 className="logo">Proportio</h4>
+        </div>
+        {props.children}
       </div>
-      {props.children}
+      <div className="header--right">
+        <a target="_blank" href="https://github.com/NateBaldwinDesign/proportio" title="Github repository">
+          <GitHubLogo size="32" color="var(--textColor"/>
+        </a>
+      </div>
     </header>
   );
 };
