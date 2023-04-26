86b9143e8fab864da6d70dd14bc1f85d8ddff41a
Nate Baldwin <natebaldwindesign@gmail.com>
2023-04-26 11:50:35 -0600

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@babel/eslint-parser',
+  extends: 'plugin:react/jsx-runtime',
+  overrides: [],
+  ignorePatterns: [
+    'package.json',
+    'package-lock.json',
+    'yarn-lock.json',
+    '**/vendor/*.js',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      json: true,
+    },
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
